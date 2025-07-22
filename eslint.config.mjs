c7091980b60ba5bae28e971ac7b58626c0a9b3f2
cheruh <cheruhjoseh@gmail.com>
2025-07-22 23:29:16 +0300

    elsint json edit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,8 @@ const eslintConfig = [
     rules: {
       'react/no-unescaped-entities': 'off',
       '@typescript-eslint/no-unused-vars': 'warn',
-      '@typescript-eslint/no-explicit-any': 'warn'
+      '@typescript-eslint/no-explicit-any': 'warn',
+      "react/no-unescaped-entities": "off"
     }
   }
 ];
